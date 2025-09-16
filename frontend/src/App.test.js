1d5ab9b7faae38064c8b12225e2a79ced904134f
Sutharsan-cp <sutharsan4106@gmail.com>
2025-09-16 23:45:04 +0530

    Folder Structure

--- frontend/src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
-
-test("renders app title", () => {
-  render(<App />);
-  expect(screen.getByText(/ML Model Comparison Dashboard/i)).toBeInTheDocument();
-});
